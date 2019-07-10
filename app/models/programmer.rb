aaab4e811d41d0d44ff20a3b89057d065958dcab
Federica Macchi <federica.macchi3@gmail.com>
2019-07-10 20:32:34 +0100

    Done.

--- app/models/programmer.rb
+++ app/models/programmer.rb
@@ -1,2 +1,10 @@
 class Programmer < ActiveRecord::Base
+
+    def attribute_full_name(attr)
+        attribute = attr.to_s
+        if attribute.include?("-")
+            attribute.sub("-", " ")
+        end
+        attribute.titleize
+    end
 end
